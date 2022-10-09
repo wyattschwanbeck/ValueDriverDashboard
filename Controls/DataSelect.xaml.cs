038a5250d56b150ef97acd36244fef0afaf39a16
wyattschwanbeck <wyattschwanbeck@gmail.com>
2022-10-09 14:19:36 -0400

    Reconfigured elements into UserControls for easier development and added initial gridsplitting functionality for views.

---
+++ Controls/DataSelect.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Yahoo.Finance;
+using ValueDriverDashboard.ControlEvents;
+
+namespace ValueDriverDashboard.Controls
+{
+    /// <summary>
+    /// Interaction logic for DataSelect.xaml
+    /// </summary>
+    public partial class DataSelect : UserControl
+    {
+        public delegate void getDataEventHandler(object source, GetDataEventArg args);
+        public event getDataEventHandler getDataEvent;
+        
+        public DataSelect()
+        {
+            //getDataEvent += OnGetDataButtonClicked;
+            InitializeComponent();
+        }
+
+        private void startDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void endDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private async void btnGetStock_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //OnGetDataButtonClicked();
+            if (this.startDatePicker != null && this.endDatePicker != null &&
+               this.txtTickerInput.Text.Length > 0 &&
+                   this.startDatePicker.SelectedDate < this.endDatePicker.SelectedDate)
+            {
+                ControlEvents.GetDataEventArg dataEvent = new ControlEvents.GetDataEventArg();
+                dataEvent.endDate = (DateTime)this.endDatePicker.SelectedDate;
+                dataEvent.startDate = (DateTime)this.startDatePicker.SelectedDate;
+                dataEvent.ticker = this.txtTickerInput.Text;
+                getDataEvent(this, dataEvent);
+            }
+            
+        }
+        public delegate GetDataEventArg getDataDelegate();
+
+
+    }
+}
---
+++ DashboardElements/StockPriceDash.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using LiveCharts;
+using LiveCharts.Wpf;
+using NodaTime;
+using System.Globalization;
+using Yahoo.Finance;
+using ValueDriverDashboard.Controls;
+using System.Linq;
+using ValueDriverDashboard.ControlEvents;
+
+namespace ValueDriverDashboard.DashboardElements
+{
+    public partial class StockDashboard : UserControl
+    {
+        public List<string> StockChartLabels { get; set; }
+        public Func<double, string> StockYFormatter { get; set; }
+        public Func<DateTime, string> XFormatter { get; set; }
+        public SeriesCollection StockChartSeriesCollection { get; set; }
+        private HistoricalDataProvider yahooDl;
+
+        public delegate void dataInputChanged(GetDataEventArg e);
+
+        public StockDashboard()
+        {
+
+            yahooDl = new HistoricalDataProvider();
+            StockChartSeriesCollection = new SeriesCollection
+            {
+
+            };
+
+            StockChartLabels = new List<string>();
+            StockYFormatter = value => value.ToString("C");
+            //XFormatter = value => value.ToString("mm/dd/yy");
+
+            InitializeComponent();
+            
+
+
+            DataContext = this;
+        }
+
+       
+
+        public async void OnDataInputAsync(object source, GetDataEventArg pullData)
+        {
+            this.StockChartSeriesCollection.Clear();
+            await yahooDl.DownloadHistoricalDataAsync(pullData.ticker, pullData.startDate, pullData.endDate);
+
+            StockChartSeriesCollection.Add(new LineSeries
+            {
+                Title = pullData.ticker,
+                Values = new ChartValues<double>(),
+                //DataLabels = true
+
+            });
+            //Labels = new List<string>();
+            int latestIndex = StockChartSeriesCollection.Count;
+
+            for (int i = 0; i < yahooDl.HistoricalData.Length; i++)
+            {
+                this.StockChartLabels.Add(yahooDl.HistoricalData[i].Date.ToString("MM/dd/yy"));
+                StockChartSeriesCollection[latestIndex - 1].Values.Add((double)yahooDl.HistoricalData[i].AdjustedClose);
+            }
+            
+        }
+
+
+        
+       
+    }
+}
